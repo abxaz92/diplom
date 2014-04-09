b0875e1fe26e791032ced9fc8a866fb299c00a56
David Chaava <chaava01@gmail.com>
2014-04-09 22:05:58 +0400

    Рефакторин кода под ООП. Добавлено отображение длины рисуемой линии

--- public/resources/js/painter.js
+++ public/resources/js/painter.js
@@ -1,55 +1,70 @@
-line = null;
-initDrawing = function(container){
-	stage = new Kinetic.Stage({
-		container: container,
-		width: 500,
-		height: 500
-	});
-	layer = new Kinetic.Layer();
-		stage.add(layer);
-
-    background = new Kinetic.Rect({
+function Painter(container){
+    var self = this;
+    this.container = container;
+    this.line = null;
+    this.moving = false;
+    this.stage = new Kinetic.Stage({
+        container: container,
+        width: 500,
+        height: 500
+    });
+    this.layer = new Kinetic.Layer();
+    this.stage.add(this.layer);
+    this.background = new Kinetic.Rect({
         x: 0, 
         y: 0, 
-        width: stage.getWidth(),
-        height: stage.getHeight(),
+        width: this.stage.getWidth(),
+        height: this.stage.getHeight(),
         fill: "white"
     });
-
-    layer.add(background);
-    stage.add(layer);
-    
-    moving = false;
-
-    background.on("mousedown", function(){
-            moving = true;
-            line = new Kinetic.Line({
+    this.text = new Kinetic.Text({
+        x: 10,
+        y: 10,
+        fontFamily: 'Calibri',
+        fontSize: 24,
+        text: '',
+        fill: 'black'
+    })
+    this.layer.add(this.background);
+    this.layer.add(this.text);
+    this.stage.add(this.layer);
+}
+Painter.prototype.bindEvents = function(){
+    var self = this;
+    this.background.on("mousedown", function(){
+            self.moving = true;
+            self.line = new Kinetic.Line({
                 stroke: "red"
             });
-            var mousePos = stage.getPointerPosition();
-                line.points([ mousePos.x, mousePos.y, mousePos.x, mousePos.y]);
-                start = {x:mousePos.x, y:mousePos.y}
-                layer.drawScene();            
-
+            var mousePos = self.stage.getPointerPosition();
+            self.line.points([ mousePos.x, mousePos.y, mousePos.x, mousePos.y]);
+            self.start = {x:mousePos.x, y:mousePos.y}
+            self.layer.drawScene();
     });
 
-    background.on("mousemove", function(){
-        if(moving){
-            layer.add(line);
-            var mousePos = stage.getPointerPosition();
-            console.log("x",Math.abs(mousePos.x - start.x));
-            console.log("y",Math.abs(mousePos.y - start.y));
-            if( Math.abs(mousePos.x - start.x) < Math.abs(mousePos.y - start.y)){
-                line.points()[3] = mousePos.y;
+    this.background.on("mousemove", function(){
+        if(self.moving){
+            self.layer.add(self.line);
+            var mousePos = self.stage.getPointerPosition();
+            if( Math.abs(mousePos.x - self.start.x) < Math.abs(mousePos.y - self.start.y)){
+                self.line.points()[3] = mousePos.y;
+                self.writeMessage("Длина : " + Math.sqrt( Math.pow(mousePos.x - self.start.x,2)  + Math.pow(mousePos.y - self.start.y,2)));
             } else{
-                line.points()[2] = mousePos.x;
+                self.writeMessage("Длина : " + Math.sqrt( Math.pow(mousePos.x - self.start.x,2)  + Math.pow(mousePos.y - self.start.y,2)));
+                self.line.points()[2] = mousePos.x;
             }
 
-            layer.drawScene();
+            self.layer.drawScene();
         }
     });
 
-    background.on("mouseup", function(){
-        moving = false;            
-    });	
+    this.background.on("mouseup", function(){
+        self.moving = false;        
+        self.writeMessage("");    
+    }); 
+}
+
+Painter.prototype.writeMessage = function(message){
+    this.text.setText(message);
+    this.layer.draw();
 }
